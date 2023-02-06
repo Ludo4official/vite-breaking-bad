344a0ec50a682f885be6cb92a8adccfe2e8cedaa
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-02-06 16:31:07 +0100

    Inizio lavoro yu gi oh

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+  name:'App',
+  components: {
+
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<style lang="scss">
+@import './styles/partials/main.scss';
+</style>
